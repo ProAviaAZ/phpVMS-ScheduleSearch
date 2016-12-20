85828c92afd52ff22a7a661ec5671e93c5aeea8f
web541 <web541php@gmail.com>
2016-12-20 12:10:15 +1100

    Updated for phpVMS 5.5.x
    Updated files for compatibility with phpVMS 5.5.x

--- core/modules/FrontSchedules/FrontSchedules.php
+++ core/modules/FrontSchedules/FrontSchedules.php
@@ -18,53 +18,53 @@ class FrontSchedules extends CodonModule
 	*
 	**/
 	
-	public function index() {
-		if(isset($this->post->action)) {
-			switch($this->post->action) {
-				case 'search':
-				$this->search_schedules();
-				break;	
-			}
-		} else {
-			$this->set('allairports', OperationsData::GetAllAirports());
-			$this->set('allairlines', OperationsData::getAllAirlines());
-			$this->set('allaircraft', FrontSchedulesData::findaircrafttypes());
-			$this->show('airport_search');
+    public function index() {
+	if(isset($this->post->action)) {
+		switch($this->post->action) {
+			case 'search':
+			$this->search_schedules();
+			break;	
 		}
+	} else {
+		$this->set('allairports', OperationsData::GetAllAirports());
+		$this->set('allairlines', OperationsData::getAllAirlines());
+		$this->set('allaircraft', FrontSchedulesData::findaircrafttypes());
+		$this->show('airport_search');
+	}
      }
 
     public function search_schedules() {
-		$arricao = DB::escape($this->post->arricao);
+	$arricao = DB::escape($this->post->arricao);
         $depicao = DB::escape($this->post->depicao);
         $airline = DB::escape($this->post->airline);
         $aircraft = DB::escape($this->post->aircraft);
                 
-		if(!$airline) {
-			$airline = '%';
+	if(!$airline) {
+		$airline = '%';
+	}
+	if(!$arricao) {
+		$arricao = '%';
+	}
+	if(!$depicao) {
+		$depicao = '%';
+	}
+	if($aircraft !== '') {
+		$aircrafts = FrontSchedulesData::findaircraft($aircraft);
+		foreach($aircrafts as $aircraft) {
+			$route = FrontSchedulesData::findschedules($arricao, $depicao, $airline, $aircraft->id, $this->enabled);
+			if(!$route) { $route = array(); }
+			if(!$routes) { $routes = array(); }
+			$routes = array_merge($routes, $route);
 		}
-		if(!$arricao) {
-			$arricao = '%';
-		}
-		if(!$depicao) {
-			$depicao = '%';
-		}
-		if($aircraft !== '') {
-			$aircrafts = FrontSchedulesData::findaircraft($aircraft);
-			foreach($aircrafts as $aircraft) {
-				$route = FrontSchedulesData::findschedules($arricao, $depicao, $airline, $aircraft->id, $this->enabled);
-				if(!$route) { $route = array(); }
-				if(!$routes) { $routes = array(); }
-				$routes = array_merge($routes, $route);
-			}
-		} else {
-			$routes = FrontSchedulesData::findschedule($arricao, $depicao, $airline, $this->enabled);
-		}
+	} else {
+		$routes = FrontSchedulesData::findschedule($arricao, $depicao, $airline, $this->enabled);
+	}
 
-		$this->set('schedule_list', $routes);
-		$this->show('schedule_results');
-	}
+	$this->set('schedule_list', $routes);
+	$this->show('schedule_results');
+    }
 	
-	public function version() {
-		echo 'Front Schedules Module Current Version: <strong>'.$this->version.'</strong>';	
-	}
+    public function version() {
+	echo 'Front Schedules Module Current Version: <strong>'.$this->version.'</strong>';	
+    }
 }
